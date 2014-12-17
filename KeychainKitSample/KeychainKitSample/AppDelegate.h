75f3fed1da02ff6aee51bee84c99e9b69cd08c11
david <davidlivadaru@gmail.com>
2014-12-17 21:10:19 +0200

    Project sample setup

---
+++ KeychainKitSample/KeychainKitSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeychainKitSample
+//
+//  Created by david on 17/12/14.
+//  Copyright (c) 2014 David Live Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeychainKitSample/KeychainKitSample/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  KeychainKitSample
+//
+//  Created by david on 17/12/14.
+//  Copyright (c) 2014 David Live Org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
